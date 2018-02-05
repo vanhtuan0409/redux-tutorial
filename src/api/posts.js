fbb1217c5a78811c50b1a93b6678baeaea6d9158
Tuấn Vương <vanhtuan0409@gmail.com>
2018-02-05 13:49:41 +0700

    change default data

--- src/api/posts.js
+++ src/api/posts.js
@@ -4,22 +4,22 @@ export default {
       {
         id: 1,
         title: "Post 1",
-        body: "Sample body"
+        body: "Sample body 1"
       },
       {
         id: 2,
         title: "Post 2",
-        body: "Sample body"
+        body: "Sample body 2"
       },
       {
         id: 3,
         title: "Post 3",
-        body: "Sample body"
+        body: "Sample body 3"
       },
       {
         id: 4,
         title: "Post 4",
-        body: "Sample body"
+        body: "Sample body 4"
       }
     ]);
   }
